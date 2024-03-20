6676dcab5ef9e1bde674bcff8f29518d886ec19a
Yassed Matta <yassed883@gmail.com>
2024-03-20 13:38:18 -0500

    Corrigiendo errores de despliegue #5

--- static/scripts.js
+++ static/scripts.js
@@ -1,28 +1,37 @@
 console.log("Prueba de JS");
 
-var socket = io.connect("wss://" + document.domain + ":" + location.port, {
-  secure: true,
-  transports: ["websocket"],
-});
+var socket = null;
 
-socket.on("connect", function () {
-  console.log("Conexión establecida con el servidor WebSocket");
-});
+function connectWebSocket() {
+  socket = io.connect("wss://" + document.domain + ":" + location.port, {
+    secure: true,
+    transports: ["websocket"],
+  });
 
-document.getElementById("messageForm1").onsubmit = function (event) {
-  event.preventDefault();
-  var messageInput = document.getElementById("messageInput");
-  socket.emit("message", messageInput.value);
-  messageInput.value = "";
-};
+  socket.on("connect", function () {
+    console.log("Conexión establecida con el servidor WebSocket");
+  });
 
-socket.on("message", function (msg) {
-  var ul = document.getElementById("messages");
-  var li = document.createElement("li");
-  li.appendChild(document.createTextNode(msg));
-  ul.appendChild(li);
-});
+  socket.on("message", function (msg) {
+    var ul = document.getElementById("messages");
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(msg));
+    ul.appendChild(li);
+  });
 
-socket.on("disconnect", function () {
-  console.log("Conexión WebSocket cerrada");
-});
+  socket.on("disconnect", function () {
+    console.log("Conexión WebSocket cerrada. Intentando reconectar...");
+    // Intentar reconectar después de un breve período de tiempo (500 ms)
+    setTimeout(connectWebSocket, 500);
+  });
+
+  document.getElementById("messageForm1").onsubmit = function (event) {
+    event.preventDefault();
+    var messageInput = document.getElementById("messageInput");
+    console.log("Mensaje enviado: " + messageInput.value);
+    socket.emit("message", messageInput.value);
+    messageInput.value = "";
+  };
+}
+
+connectWebSocket();
